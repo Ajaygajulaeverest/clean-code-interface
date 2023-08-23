cb66315ed334e01a5b4be5fe3b8cfc3197f28dda
ajaygajulaeverest <ajay.gajula@everest.engineering>
2023-08-23 14:37:42 +0530

    unwanted data is removed

--- importing_files/xmldata.js
+++ importing_files/xmldata.js
@@ -3,7 +3,6 @@ var fs = require("fs");
 var xml2js = require("xml2js");
 function xmldataimport() {
   let results = [];
-  //   var parser = new xml2js.Parser();
 
   data = fs.readFileSync(
     "/Users/ajaygajula/Documents/clean-code/Assignment-2/data/persons.xml"
@@ -19,25 +18,8 @@ function xmldataimport() {
   });
 
   return results;
-  //   console.log(results);
+
 }
 xmldataimport();
 module.exports = { xmldataimport };
 
-// const fs = require("fs");
-// var convert = require("xml-js");
-// var xml = require("fs").readFileSync("./person.xml", {
-//   encoding: "utf8",
-//   flag: "r",
-// });
-// // console.log(xml);
-
-// var xmlData = convert.xml2json(xml, {
-//   compact: true,
-//   space: 4,
-// });
-// // array = [];
-// fs.writeFileSync("output2.json", xmlData);
-// const sample = require("./output2.json");
-
-// console.log(sample.People.Person);
